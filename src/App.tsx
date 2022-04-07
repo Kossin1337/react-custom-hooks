920fc5aaac127d345fc02b911b06c136e3084afd
Kossin1337 <piotrswobodzinski@gmail.com>
2022-04-07 05:29:35 +0200

    useWindowSize hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import { useWindowSize } from "./hooks/useWindowSize";
 
 function App() {
+  const { width, height } = useWindowSize();
+
   return (
-    <div className="ppp">
+    <div className="app">
       <span className="title">Custom Hooks</span>
+      <span>{`useWindowSize: ${width} / ${height} [px]`}</span>
     </div>
   );
 }
--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -25,4 +25,6 @@ export const useWindowSize = () => {
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  return windowSize;
 };
